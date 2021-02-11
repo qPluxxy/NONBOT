4ae9b844a58eb69b206b3dfe65ba04dce78b502d
qPluxxy <78903156+qPluxxy@users.noreply.github.com>
2021-02-11 18:17:01 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext.commands import bot, Bot
+
+bot = Bot(command_prefix='논!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} 에 로그인하였습니다!')
+
+@bot.command()
+async def 주인(ctx):
+    await ctx.send('qPluxxy')
+
+@bot.command()
+async def 안녕(ctx):
+    await ctx.send('반....갑....다........')
+
+@bot.command()
+async def 핑(ctx):
+    await ctx.send('퐁! {0}'.format(round(bot.latency*1000))+'ms')
+
+bot.run('Nzk3MzI1MTQ4NDU2NzQ3MDE5.X_k06A.03pwWV215tBXspgGcdWDO7rwZCE')
